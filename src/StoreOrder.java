d8d41af4ce7c5e8d70625c14ca2c75472fdcbf94
Srivas Tirumala <srivastirumala@Srivass-MacBook-Pro.local>
2017-09-12 22:49:40 -0700

    pizza order

--- src/StoreOrder.java
+++ src/StoreOrder.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
 
 public class StoreOrder {
-
+	private ArrayList<Object> orderArrayList;
+	
+	public StoreOrder() {
+		orderArrayList = new ArrayList<Object>();
+	}
+	
+	public void addOrder(Object object) {
+		orderArrayList.add(object);
+	}
+	
+	public int getNumOrders() {
+		return orderArrayList.size();
+	}
+	
+	public double getTotalCost() {
+		double cost = 0.0;
+		for (int i = 0; i < orderArrayList.size(); i++) {
+			cost += orderArrayList.get(i).PRICE;
+		}
+		return cost;
+	}
 }
